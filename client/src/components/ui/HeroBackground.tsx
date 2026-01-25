48b766fad6b114fd08cbbc31930232f9a543ba16
Sarthak Goyal <sarthakgoyal487@gmail.com>
2026-01-25 20:36:08 +0530

    fix: using image link in hero backgound instead of file.

--- client/src/components/ui/HeroBackground.tsx
+++ client/src/components/ui/HeroBackground.tsx
@@ -1,6 +1,8 @@
 const BACKGROUND_IMAGES = {
-  mobile: "./pic-mobile.webp",
-  desktop: "./pic-desktop.webp",
+  mobile:
+    "https://images.unsplash.com/photo-1648995361141-30676a75fd27?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  desktop:
+    "https://images.unsplash.com/photo-1599058917212-d750089bc07e?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
 
 // Component: Hero Section Background
